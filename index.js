f69f6f77e84d3ba351c85cf63aabd5e2dad74003
Jose-17-11 <ariashdez@outlook.com>
2024-11-09 17:24:49 -0600

    add gitignore

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { pool } from './db.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello world")
